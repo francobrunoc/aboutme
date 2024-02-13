e227013e9255d4cbb3766ef35311ff793311241d
franco.corteletti <franco.corteletti@clamed.com.br>
2024-02-12 22:11:30 -0300

    first commit

---
+++ assets/languages.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    code: 'en',
+    name: 'English',
+    cname: '英语',
+    ename: 'English',
+  },
+  {
+    code: 'pt',
+    name: 'Português',
+    cname: '葡萄牙语',
+    ename: 'Português',
+  }
+]
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <main>
+    <v-google-translate @select="googleTranslateSelectedHandler" />
+    <Curriculum />
+  </main>
+</template>
+
+<script>
+import Vue from "vue";
+import vGoogleTranslate from "v-google-translate";
+import languages from "assets/languages";
+import Curriculum from "../components/Curriculum.vue";
+
+Vue.use(vGoogleTranslate);
+
+export default {
+  name: "IndexPage",
+  components: { Curriculum },
+  data() {
+    return {
+      languages,
+    };
+  },
+  methods: {
+    googleTranslateSelectedHandler(language) {
+      const { code, name, cname, ename } = language;
+      // todo ...
+    },
+  },
+};
+</script>
